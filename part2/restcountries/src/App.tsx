5d0d8f6035b0ba48f72ead8dd2b6907773fa1a3f
geniusjoelraj <geniusjoel2005@gmail.com>
2025-11-22 16:49:38 +0530

    part 2 optional complete

--- part2/restcountries/src/App.tsx
+++ part2/restcountries/src/App.tsx
@@ -10,15 +10,17 @@ function App() {
         setCountries(countries);
       })
   }, [])
+  const apiKey = import.meta.env.VITE_API_KEY;
 
   const getCountry = (query: string) => {
     axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${query}`)
       .then(res => {
-        setCountry(res.data.name.official);
+        setCountry(res.data.name.common);
         setCapital(res.data.capital);
         setArea(res.data.area);
         setLanguages(Object.values(res.data.languages));
         setFlag(res.data.flags.png);
+        getWeather(res.data.latlng);
       })
   }
   const [searchQuery, setSearchQuery] = useState('');
@@ -28,6 +30,9 @@ function App() {
   const [area, setArea] = useState<number>();
   const [languages, setLanguages] = useState<Array<string>>();
   const [flag, setFlag] = useState<string>();
+  const [temp, setTemp] = useState<string>();
+  const [imgUrl, setImgUrl] = useState<string>()
+  const [wind, setWind] = useState<number>();
   const handleChange = (val: any) => {
     setSearchQuery(val.target.value)
   }
@@ -35,10 +40,22 @@ function App() {
     .filter(country =>
       country.toLowerCase().includes(searchQuery.toLowerCase())
     ) : [];
+  const getWeather = (latlong: Array<number>) => {
+    if (latlong) {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latlong[0]}&lon=${latlong[1]}&appid=${apiKey}`)
+        .then((res => res.data))
+        .then(data => {
+          setTemp((data.main.temp - 273).toFixed(2).toString() + "°C")
+          setImgUrl(`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+          setWind(data.wind.speed);
+        });
+
+    }
+  }
 
   useEffect(() => {
     if (filteredCountries.length == 1) {
-      getCountry(filteredCountries[0])
+      getCountry(filteredCountries[0]);
     }
     setCountry('')
   }, [searchQuery])
@@ -51,14 +68,17 @@ function App() {
           {filteredCountries.length <= 10 ?
             filteredCountries
               .map(filteredCountry => (
-                <li key={filteredCountry}>{filteredCountry}</li>
+                <li key={filteredCountry}>
+                  {filteredCountry}
+                  <button onClick={() => getCountry(filteredCountry)}>show</button>
+                </li>
               ))
             : null
           }
         </ul>
         : <p>Loading...</p>
       }
-      {filteredCountries.length == 1 ?
+      {filteredCountries ?
         <>
           <h1>{country}</h1>
           <p>Capital: {capital}</p>
@@ -68,6 +88,10 @@ function App() {
             {languages?.map(lang => <li>{lang}</li>)}
           </ul>
           <img src={flag} alt="flag" />
+          <h1>Weather in {country}</h1>
+          <p>Temperature: {temp}</p>
+          <img src={imgUrl} alt={imgUrl} />
+          <p>Wind: {wind} meter/sec</p>
         </>
         :
         null
