a2dfc7039015ff3377af8d29f3600f4f28530f38
geniusjoelraj <geniusjoel2005@gmail.com>
2025-12-03 21:36:35 +0530

    all tests complete

--- part4/blog/controllers/blog.js
+++ part4/blog/controllers/blog.js
@@ -1,18 +1,28 @@
 const blogRouter = require('express').Router()
+const { default: mongoose } = require('mongoose')
 const Blog = require('../models/blog')
+const { json } = require('express')
 
-blogRouter.get('/', async (req, res) => {
-  const data = await Blog.find({})
-  res.json(data)
+blogRouter.get('/', async (req, res, next) => {
+  try {
+    const data = await Blog.find({})
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
 })
 
 blogRouter.get('/:id', async (req, res, next) => {
-  const id = req.params.id
-  const blog = await Blog.findById({ id })
-  if (blog) {
-    res.json(blog)
-  } else {
-    res.status(404).end()
+  const _id = req.params.id
+  const blog = await Blog.findById({ _id })
+  try {
+    if (blog) {
+      res.json(blog)
+    } else {
+      res.status(404).end()
+    }
+  } catch (err) {
+    next(err)
   }
 })
 
@@ -24,8 +34,37 @@ blogRouter.post('/', async (req, res, next) => {
     url: body.url,
     likes: body.likes,
   })
-  const savedBlog = await newBlog.save()
-  res.json(savedBlog)
+  try {
+    const savedBlog = await newBlog.save()
+    res.json(savedBlog)
+  } catch (err) {
+    next(err)
+  }
+})
+
+blogRouter.patch('/:id', async (req, res, next) => {
+  const _id = req.params.id
+  const body = req.body
+  try {
+    const updated = await Blog.findOneAndUpdate({ _id }, body);
+    res.json(updated)
+  } catch (err) {
+    next(err)
+  }
+})
+
+blogRouter.delete('/:id', async (req, res, next) => {
+  const _id = req.params.id
+  try {
+    const deleted = await Blog.findOneAndDelete({ _id });
+    if (!deleted) {
+      res.status(404)
+    } else {
+      res.json(deleted)
+    }
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = blogRouter
--- part4/blog/tests/blog_api.js
+++ part4/blog/tests/blog_api.js
@@ -4,6 +4,8 @@ const supertest = require('supertest')
 const app = require('../app')
 const assert = require('assert')
 const Blog = require('../models/blog')
+const { requestLogger } = require('../utils/middleware')
+const { title } = require('process')
 
 const api = supertest(app)
 
@@ -32,38 +34,38 @@ beforeEach(async () => {
 })
 
 describe('testing get', async () => {
-  // test('blogs are retured as json', async () => {
-  //   await api
-  //     .get('/api/blog')
-  //     .expect(200)
-  //     .expect('Content-type', /application\/json/)
-  // })
-  // test('getting all blogs', async () => {
-  //   const res = await api
-  //     .get('/api/blog')
-  //     .expect(200)
-  //   assert.strictEqual(res.body.length, 2)
-  // })
-  // test('a specific blog is retured', async () => {
-  //   const res = await api
-  //     .get('/api/blog')
-  //     .expect(200)
-  //   const titles = res.body.map(e => e.title)
-  //   assert(titles.includes('My first blog'))
-  // })
-  // test('unique identifier property', async () => {
-  //   const res = await api
-  //     .get('/api/blog')
-  //     .expect(200)
-  //   assert(Object.keys(res.body[0]).includes("id"))
-  //
-  // })
-  // test('likes property is missing', async () => {
-  //   const res = await api
-  //     .get('/api/blog')
-  //     .expect(200)
-  //   assert(Object.keys(res.body[0]).includes('likes'))
-  // })
+  test('blogs are retured as json', async () => {
+    await api
+      .get('/api/blog')
+      .expect(200)
+      .expect('Content-type', /application\/json/)
+  })
+  test('getting all blogs', async () => {
+    const res = await api
+      .get('/api/blog')
+      .expect(200)
+    assert.strictEqual(res.body.length, 2)
+  })
+  test('a specific blog is retured', async () => {
+    const res = await api
+      .get('/api/blog')
+      .expect(200)
+    const titles = res.body.map(e => e.title)
+    assert(titles.includes('My first blog'))
+  })
+  test('unique identifier property', async () => {
+    const res = await api
+      .get('/api/blog')
+      .expect(200)
+    assert(Object.keys(res.body[0]).includes("id"))
+
+  })
+  test('likes property is missing', async () => {
+    const res = await api
+      .get('/api/blog')
+      .expect(200)
+    assert(Object.keys(res.body[0]).includes('likes'))
+  })
 })
 
 describe('testing post', async () => {
@@ -87,7 +89,7 @@ describe('testing post', async () => {
       .send(newBlog)
       .expect(200)
   })
-  test('post has no title', async () => {
+  test('post has no url', async () => {
     const newblog = {
       "title": "my first blog",
       "likes": 10
@@ -106,6 +108,62 @@ describe('testing post', async () => {
       .post('/api/blog')
       .send(newblog)
       .expect(400)
+  })
+  test('post has no title and url', async () => {
+    const newblog = {
+      "likes": 10
+    }
+    await api
+      .post('/api/blog')
+      .send(newblog)
+      .expect(400)
+  })
+})
+
+describe('delete and update', async () => {
+  test("updating likes", async () => {
+    const update = {
+      likes: 10
+    }
+    const testBlog = await Blog.findOne()
+    const id = testBlog.id
+    await api
+      .patch(`/api/blog/${id}`)
+      .expect(200)
+      .send(update)
+    const updatedBlog = await Blog.findById(id)
+    assert.strictEqual(updatedBlog.likes, 10)
+  })
+  test("updating title", async () => {
+    const update = {
+      title: "Hello world"
+    }
+    const testBlog = await Blog.findOne()
+    const id = testBlog.id
+    await api
+      .patch(`/api/blog/${id}`)
+      .expect(200)
+      .send(update)
+    const updatedBlog = await Blog.findById(id)
+    assert.strictEqual(updatedBlog.title, "Hello world")
+  })
+
+  test("deleting one", async () => {
+    const testBlog = await Blog.findOne()
+    const id = testBlog.id
+    await api
+      .delete(`/api/blog/${id}`)
+      .expect(200)
+    const updatedBlogs = await Blog.find()
+    assert.strictEqual(updatedBlogs.length, 1)
+  })
+  test("deleting non existing blog", async () => {
+    const id = 123
+    const res = await api
+      .delete(`/api/blog/${id}`)
+      .expect(400)
+    const updatedBlogs = await Blog.find()
+    assert.strictEqual(updatedBlogs.length, 2)
   })
 })
 
